fae0d1e38575bc669f226dde401ad287227bd488
Abhishek Sangwan <Linux@Abhisheks-MacBook-Pro.local>
2015-12-10 16:34:43 +0530

    functional test added for controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: :true
   validates :price, numericality: {greater_than_or_equal: 0.01}
   validates :image_url, allow_blank: :true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a url for gif,png or jpg'}
-
+  
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
