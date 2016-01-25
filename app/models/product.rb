9d7d08421f95f20aa2602ea722d150edbccf3323
Abhishek Sangwan <Linux@Abhisheks-MacBook-Pro.local>
2016-01-25 17:27:43 +0530

    number of line items in cart feature added

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,20 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal: 0.01}
   validates :image_url, allow_blank: :true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a url for gif,png or jpg'}
   
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   def self.latest
     Product.order(:updated_at).last
+  end
+  private
+  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items Present')
+      return false
+    end
   end
 end
